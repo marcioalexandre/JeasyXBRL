b29fd0fa58bff9a1e64b4092fd27c7fa235d0638
Marcio Alexandre <marcio.alexandre83@gmail.com>
2015-10-07 17:48:44 -0300

    Update JeasyXbrl.java

--- src/jeasyxbrl/JeasyXbrl.java
+++ src/jeasyxbrl/JeasyXbrl.java
@@ -1,8 +1,18 @@
 /**
- This software is subject to the terms of the GNU GENERAL PUBLIC LICENSE
- Agreement, available at the following URL:
- http://www.gnu.org/licenses/gpl.txt.
- You must accept the terms of that agreement to use this software.
+This file is part of jeasyXBRL.
+
+jeasyXBRL is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+jeasyXBRL is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with jeasyXBRL.  If not, see <http://www.gnu.org/licenses/>.
 
  Copyright (C) 2015 Marcio Alexandre Pereira da Silva
  All Rights Reserved.
@@ -10,8 +20,8 @@
  * @author Marcio Alexandre
  * @email marcio.alexandre83@gmail.com
  * @since 2015-09-22
- * @version beta0.0
- */
+ * @version 2.0-beta
+*/
 
 package jeasyxbrl;
 
